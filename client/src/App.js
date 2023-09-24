74bf7389c9d04fe6350685edac7b5b6642cb6c2f
BColeWebDev <Briancole1994@hotmail.com>
2023-09-24 10:27:45 -0400

    feature:frontend-UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Table from './component/Table';
-
+import { ChakraProvider, Button } from "@chakra-ui/react"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>testing</h1>
-      </header>
-      <Table/>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <h1>testing</h1>
+        </header>
+        <Table />
+
+      </div>
+    </ChakraProvider>
+
   );
 }
 
--- client/src/component/Table.jsx
+++ client/src/component/Table.jsx
@@ -1,19 +1,85 @@
 import React,{useState,useEffect, Fragment} from 'react';
 import axios from "axios"
-const Table = () => {
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    Spinner,
+    TableCaption,
+    Box,
+    TableContainer,
+    Button,Text, Select, 
+  } from '@chakra-ui/react'
+const Tables = () => {
     const [data, setdata] = useState();
-
+    const [page, setpage] = useState(1);
+    const [pageItems, setpageItems] = useState(100);
+    const [isLoading, setIsLoading] = useState(false);
+    const [input, setinput] = useState("");
     useEffect(() => {
-    axios.get('http://localhost:5000/health')
-    axios.get('http://localhost:5000/api/users/?page=1&pageSize=100').then((val)=>setdata(val.data))
-    }, []);
-    return <>User
-    <div>
-    {data?.data.map((val,idx) => <Fragment key={idx}>
-        <p>{val.name}</p>
-    </Fragment>)}
-    </div>
+        setIsLoading(true)
+    axios.get('http://127.0.0.1:5000/health')
+
+    axios.get(`http://127.0.0.1:5000/api/users/?page=${page}&pageSize=${pageItems}`).then((val)=>{
+ setIsLoading(false)
+    setdata(val.data)
+})
+    }, [page,pageItems]);
+    console.log(data)
+    return <>
+    <Box>
+        
+    <TableContainer>
+  <Table variant="striped">
+  <TableCaption position={"fixed"} bottom="0" bg={"gray.300"} w={"100%"}><Box display={"flex"} alignItems={"center"} w={"100%"} justifyContent={"center"}>
+
+  <Button  mx={4}  isDisabled={page <= 1? true : false} onClick={()=>setpage(1)}>{"<<"}</Button>
+   <Button isDisabled={page <= 1? true : false} onClick={()=>setpage( page <= 1 ? 1 : page -1)}>{"<"}</Button>
+
+
+ <Box  display={"flex"}alignItems={"center"}>
+    <Text mx={2}>{data?.pageNumber} out of {data?.totalPages}</Text>
+   
+    <Select  bg={"white"} mx={4} w={"200px"} onChange={(e)=>{
+        setIsLoading(true)
+        setpageItems(e.target.value)
+    }}>
+    {[10,25,100,1000].map((val)=><option key={val}value={val} >Display{val}</option>)}
+    </Select>
+ </ Box>
+
+<Button isDisabled={page === data?.totalPages ? true :false}  mx={4} onClick={()=>setpage( page  + 1)}>{">"}</Button>
+<Button  isDisabled={page === data?.totalPages ? true :false}  mx={4} onClick={()=>setpage(data?.totalPages)}>{">>"}</Button>
+
+
+</Box>
+</TableCaption>
+    <Thead position={"fixed"} top="0" bg={"yellow"} w={"100%"} display={"flex"} justifyContent={"space-between"}>
+    <Tr display={"flex"} width={"100%"}>
+        
+        {data?.tableHeaders?.map((val,idx)=>
+        
+            <Th flex={0.75} textAlign={"center"} key={idx}>{val.name}</Th>
+       )}
+    </Tr>
+      
+    </Thead>
+    {isLoading ? <Box minH={"100vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}><Spinner/> </Box>:    <Tbody maxH={"100px"} overflow={"auto"}>
+     {data?.data?.map((tableBody,idx)=><>
+        <Tr key={idx}>{data.tableHeaders.map((val,key)=> <Td key={key}>{tableBody[`${val.value}`]}</Td>)}</Tr>
+     </>)}  
+    </Tbody>}
+ 
+  </Table>
+</TableContainer>
+
+
+
+    </Box>
      </>;
 }
  
-export default Table;
+export default Tables;
